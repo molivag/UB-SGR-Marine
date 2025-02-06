c52b4a9b75700bd5d1802abf10352e0eb46a3247
molivag FedOX <maoliva@cimne.upc.edu>
2025-02-06 23:54:18 +0100

    main y router - Ultima version estable de router y main

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import AppRouter from './router'
 import './styles/index.css'
+
+import AppRouter from './router'
+
+//Este es el global que podria importarse igualmente en index.html
+//Aqui deberia de cargarse el import de google fonts o en el head de index.html. 
+//Sería lo mismo?
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/router.tsx
+++ src/router.tsx
@@ -1,26 +1,25 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout		from './layouts/layout'		;
+import Layout from "./layouts/layout";
 
+import IndexPage from "./views/IndexView";
+// import HomePage 	from './views/HomeView'	;
+import PeoplePage from "./views/PeopleView";
+import ProjectsPage from "./views/ProjectsView";
+import GroupPage from "./views/GroupsView";
 
-import IndexPage 	from './views/IndexView';
-import HomePage 	from './views/HomeView'	;
-import PeoplePage 	from './views/PeopleView'	;
-import ProjectsPage 	from './views/ProjectsView';
-
-function AppRouter(  ) {
-  return(
+function AppRouter() {
+  return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<IndexPage />} index/>
-        <Route element={<Layout/>}>
-          <Route path="Home" element={<HomePage />} />
-          <Route path="People" element={<PeoplePage />}/>
-	  <Route path="Projects" element={<ProjectsPage/>}/>
-        </Route> 
+        <Route element={<Layout />}>
+          <Route path="/" element={<IndexPage />} index />
+          <Route path="People" element={<PeoplePage />} />
+          <Route path="Projects" element={<ProjectsPage />} />
+          <Route path="Groups" element={<GroupPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
 
-export default AppRouter
+export default AppRouter;
