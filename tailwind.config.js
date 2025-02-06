bb5a5ba3ed2632f7fc3d26c404fa2a03bc1b22ba
molivag FedOX <maoliva@cimne.upc.edu>
2025-02-06 23:57:25 +0100

    Se agrega el archivo de configuracion de tailwind para que el coc-tailwindcss siempre se inicie de forma automatica, es un archivo necesario para el plugin pero no contiene nada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+// Solo se creo para el plugin de Coc que siempre este activo 
+// este archivo ya no se usa pues tailwind funciona con
+// el plugin de vite y los colores y de mas personalizaciones se realizan en
+// index.css
