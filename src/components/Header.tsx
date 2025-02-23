ae70c8fab7927744c14dc549f8de7dcad46d4dc0
molivag FedOX <maoliva@cimne.upc.edu>
2025-02-23 12:13:10 +0100

    HEADER - Se agrega el componente de Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import Navigation_menu from "./Navigation";
+
+function HeaderComp() {
+  return (
+    <header>
+      <div>
+        <div className=" border-4 border-dotted">
+          <Navigation_menu />
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default HeaderComp;
