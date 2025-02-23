a1b19054aecf1d9d341c762e6561f63efbbedc64
molivag FedOX <maoliva@cimne.upc.edu>
2025-02-23 12:12:55 +0100

    FOOTER - Se agrega el componente de Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import { Modal } from 'flowbite'
+
+function FooterComp() {
+  return (
+    <header>
+      <div className="border-6 border-double border-white bg-UBfothead py-20">
+
+
+      </div>
+    </header>
+  );
+}
+
+export default FooterComp;
