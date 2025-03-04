ac93b2358dab9c3d318de404d293410d9a4ba838
molivag FedOX <maoliva@cimne.upc.edu>
2025-03-04 13:57:00 +0100

    TeamMember.tsx - se agregaron propiedades css para reduicir el tamanio de la imagen en dispositivos inferiores a sm, se completaron los props en las ubicaciones necesarias del componente, falta agregar algunos otros mas y decidir que hacer con la distribucion de bio

--- src/components/TeamMember.tsx
+++ src/components/TeamMember.tsx
@@ -1,8 +1,10 @@
 // function Member({props}){    ----> entre llaves es desctructuring
+import { FaGoogleScholar } from "react-icons/fa6";
+import { FaOrcid } from "react-icons/fa";
 
 type Tmember = {
   id: number;
-  current: boolean,
+  current: boolean;
   name: string;
   category: string;
   department: string;
@@ -17,46 +19,52 @@ type Tmember = {
 };
 
 // aqui lo que hacemos es NO desestructurar el objeto prop que tiene las propiedas member(que es un objeto) y key que es un numero
-function Member( prop : {dataMember:Tmember, key:number}) {
+function Member(prop: { dataMember: Tmember; key: number }) {
   // o bein podria desestructurarlo como:
-   // function Member( {Name_prop, key}: {Name_prop:Tmember, key:number}) {
-  // -----Es decir en el caso de desestructurar el prop, el nombre que le pongamos a 
+  // function Member( {Name_prop, key}: {Name_prop:Tmember, key:number}) {
+  // -----Es decir en el caso de desestructurar el prop, el nombre que le pongamos a
   // -----prop debe ser el mismo que el declarado en el type que coincide con el valor de la prop que se esta pasando desde el componente padre, es decir, el que renderiza el componente hijo. En palabras tecnicas:
   // *** Al desestructurar props, los nombres de las variables deben coincidir con las claves del objeto props recibido desde el componente padre y con las definiciones en el tipo de TypeScript.***
 
-  
   return (
     <div>
-    {/* <div className="bg-blue-50 grid grid-row-2 gap-4"> */}
-      <div className="mx-auto py-3">
+      {/* <div className="bg-blue-50 grid grid-row-2 gap-4"> */}
+      <div className="py-3 w-full max-w-full">
         <img
-        className="w-full sm:w-40 md:w-45 lg:w-55 h-auto object-cover rounded-full"
-
+          className="mx-auto w-32  sm:w-44 md:w-48 lg:w-56  xl:w-64 h-auto object-cover rounded-full for-small for-middle"
           // className=" rounded-full w-64 h-64 "
-	src={`img/team/${prop.dataMember.imageID}.jpg`}
-          alt={`Fotografia de Miembro ${prop.dataMember.name} `}
+          src={`img/team/${prop.dataMember.imageID}.jpg`}
+          alt={`Photo of ${prop.dataMember.name} `}
         />
       </div>
       <div className="col-4 ">
         <h3>{prop.dataMember.name}</h3>
         <p>
-          <em>Category</em>: ssss
+          <em>Category</em>: {prop.dataMember.category}
         </p>
         <p>
-          <em>Department</em>: ssss
+          <em>Department</em>: {prop.dataMember.department}
         </p>
         <p>
-          <em>Research Group</em>:ssssss
+          <em>Research Group</em>: {prop.dataMember.group}
         </p>
         <p>
-          <em>e-mail</em>: sssssss
+          <em>e-mail</em>: {prop.dataMember.email}
         </p>
+        <div className="flex gap-x-2 mb-4">
+          <a
+            href={`${prop.dataMember.orcid}`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FaOrcid />
+          </a>
+          <a href="">
+            <FaGoogleScholar />
+          </a>
+        </div>
         <p>
-          <a href="">OrcID</a>
-          <a href="">Google Scholar</a>
-        </p>
-        <p>
-          <em>Bio:</em>:
+          <em>Bio</em>: {prop.dataMember.description}
         </p>
       </div>
     </div>
