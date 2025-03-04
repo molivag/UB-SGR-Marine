e2f0a2bee58aba45aded1027817a8987ec0dea71
molivag FedOX <maoliva@cimne.upc.edu>
2025-03-04 12:43:22 +0100

    vite.config.ts - Se quita el plugin de tailwind para instalarse la version anteriror

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+// import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
+    // tailwindcss(),
   ],
 })
 
