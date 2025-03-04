3daa98928c6dbd062763f5281eec7db17c09f6a8
molivag FedOX <maoliva@cimne.upc.edu>
2025-03-04 13:25:42 +0100

    HEADER - Se agrega el carrusel al componente header, el navigation menu debe cambiarse por el definitivo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,24 @@
+import MoveImages from "./Carousel";
 import Navigation_menu from "./Navigation";
+import SwipImages from "./SwiperComp";
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+import '../styles/styles.css';
+
 
 function HeaderComp() {
   return (
     <header>
       <div>
-        <div className=" border-4 border-dotted">
+        <div className="h-96 ">
+          {/* <MoveImages /> */}
+	  <SwipImages/>
+        </div>
+        <div>
           <Navigation_menu />
         </div>
       </div>
