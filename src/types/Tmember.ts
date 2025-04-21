858f7500a4bc3913c0bcf853a6514ae4455ac779
molivag FedOX <maoliva@cimne.upc.edu>
2025-04-21 20:57:19 +0200

    Se agrego el type global de Tmember

---
+++ src/types/Tmember.ts
@@ -0,0 +1,17 @@
+type Tmember = {
+  id: number;
+  current: boolean;
+  name: string;
+  category: string;
+  department: string;
+  group: string;
+  email: string;
+  orcid: string;
+  scholar: string;
+  website: string;
+  socialmedia: string;
+  imageID: string;
+  description: string;
+};
+
+export default Tmember
