5d3cb3ce8402307309fe59cbeaabe6aacf82286c
molivag FedOX <maoliva@cimne.upc.edu>
2025-05-10 10:29:56 +0100

    Se agrega el Type Tprojects

---
+++ src/types/Tproject.ts
@@ -0,0 +1,24 @@
+type Tproject = {
+  id: number;
+  email: string;
+  researcher: string;
+  piproject: string;
+  acronym: string;
+  nameProject: string;
+  projectID: string;
+  start: string;
+  end: string;
+  summary: string;
+  websiteProject: string;
+  fundingAgency: string;
+  socialNetwork_x: string;
+  socialNetwork_bsky: string;
+  socialNetwork_inst: string;
+  imageID_logo: string;
+  imageID_FunAgen: string;
+  imageID_ex1: string;
+  imageID_ex2: string;
+  imageID_ex3: string;
+};
+
+export default Tproject
