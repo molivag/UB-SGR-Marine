6f07deea1faf856ee03c1abbbccd93c332193312
molivag FedOX <maoliva@cimne.upc.edu>
2025-05-10 21:57:39 +0100

    Projects - Se agrega el componente Projects que se renderizara cuantos projectos haya en el archivo projects.xlsx, es un componente de tarjeta que cuando se da click en Read More, redirije al componente FullDescription pasando el state desde aqui Projects a el componente FullDescription para hacer este igualmente dinamico con el prop y sus parametros

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,39 +1,51 @@
 import { Fragment } from "react/jsx-runtime";
-import { Card, Button } from "flowbite-react";
+import { Card } from "flowbite-react";
 import { Link } from "react-router-dom";
+import Tproject from "../types/Tproject";
 
 
-function ProjectComp() {
+function ProjectComp(prop:{
+  dataProject: Tproject
+  key: number
+}) {
   return (
     <Fragment>
+
       <Card
-        className="max-w-sm bg-green-50"
+        className="max-w-sm bg-green-50 flex flex-col justify-between h-full"
         renderImage={() => (
           <img
-	  // src={`img/projects/${prop.dataMember.imageID}.jpg`}
-            // alt={`Photo of ${prop.dataMember.name} `}
-	    src="img/projects/logo_acidomic2.jpg"
-                alt="Logo del Proyecto"
-                className="w-full h-auto"
-              />
-            )}
-          >
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              Proyecto de Biotecnología Avanzada
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              Investigación sobre enzimas modificadas para tratamientos enzimáticos avanzados.
-            </p>
-            <div className="mt-4">
-            <Link to="/Contact">
-                <Button>
-                  Ver más
-                </Button>
-              </Link>
-            </div>
-          </Card>
-          </Fragment>
-        );
-      }
+            src={`img/projects/${prop.dataProject.imageID_logo}.jpg`}
+            alt={`Logo of the project ${prop.dataProject.projectID}`}
+            className="w-full h-auto"
+          />
+        )}
+      >
+        <div className="flex-grow border-4 border-dashed border-red-400">
+          <h5 className="text-xl text-UBfoothead font-bold tracking-tight dark:text-white">
+            {prop.dataProject.nameProject}
+          </h5>
+        </div>
+	<div>
+	<h6 className="text-UBfoothead font-semibold">Duration</h6>
+	<p>{prop.dataProject.start} - {prop.dataProject.end}</p>
+	</div>
+      
+	<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel recusandae veniam iure repudiandae, quod, tempore atque sit culpa blanditiis dolorum sapiente provident? Dicta totam atque eius doloribus vero, similique facilis!</p>
+        <div className="mt-4">
+      	<Link
+      	to={`/Projects/${prop.dataProject.acronym}`}
+      	state={{ project: prop.dataProject }}
+      	>
+      	  <hr className="border-t border-UBfoothead mb-2 max-w-32" />
+      	  <span className="font-semibold hover:text-blue-600">
+      	    Read More
+      	  </span>
+          </Link>
+        </div>
+      </Card>
+    </Fragment>
+  );
+}
 
 export default ProjectComp;
