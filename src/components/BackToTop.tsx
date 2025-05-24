0a56461585236bbbe132b24c46d2016ae77c7c2b
Marco Oliva-Gutierrez <moligut@localhost.localdomain>
2025-05-24 10:56:41 +0200

    El boton de scroll to top que regresa de vuelta al top de la pagina se cambio de nombre a BackToTop para que no sea el mismo que ScrollToTop que es el componente que ace que al dar click el enlace lleve al top de la pagina

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,33 +1,32 @@
 import { useEffect, useState } from "react";
 import { CircleChevronUp } from "lucide-react";
 
-const ScrollToTop = () => {
+const GoTop = () => {
   const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     const toggleVisibility = () => {
-      setIsVisible(window.scrollY > 500);
+      setIsVisible(window.scrollY > 600);
     };
 
     window.addEventListener("scroll", toggleVisibility);
     return () => window.removeEventListener("scroll", toggleVisibility);
   }, []);
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
+  const GoTop = () => {
+    window.scrollTo({ top: 450, behavior: "smooth" });
   };
 
   return (
     <button
-      onClick={scrollToTop}
-      className={`fixed bottom-6 right-2 z-50 p-3 bg-UBfoothead text-white rounded-full shadow-md transition-opacity ${
+      onClick={GoTop}
+      className={`fixed bottom-6 right-14 z-50 p-3 bg-orange-500 text-white rounded-full shadow-md transition-opacity ${
         isVisible ? "opacity-50" : "opacity-0"
       }`}
     >
-      <CircleChevronUp size={24} />
+      <CircleChevronUp size={34} />
     </button>
   );
 };
 
-export default ScrollToTop;
-
+export default GoTop;
