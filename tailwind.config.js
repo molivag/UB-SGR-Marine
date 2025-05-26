2f1467cb36e1b4646d0668f6fc9c66c57adc7a43
Marco Oliva-Gutierrez <moligut@localhost.localdomain>
2025-05-26 10:32:02 +0200

    Se agregan los styles tanto en index como en tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,39 +4,48 @@ const flowbite = require("flowbite-react/tailwind");
 
 export default {
   content: [
-    "./index.html", 
+    "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     flowbite.content(),
     './node_modules/preline/preline.js',
   ],
   theme: {
     extend: {
-      fontFamily:{
-	Montserrat:["Montserrat", "sans-serif"],
-	// Barlow: ["Barlow", "sans-serif"],
+      fontFamily: {
+        Montserrat: ["Montserrat", "sans-serif"],
+        // Barlow: ["Barlow", "sans-serif"],
 
       },
       colors: {
-	'UBfoothead':'#01204e',
-	'UBtitles':'#086375',
-	'UBnone':'#bedcd0',
-	'UBfondo': '#fbf2e5',
-	'UBop1':'#7A3B69',
-	'UBop2':'#5A746E',
+        'UBfoothead': '#01204e',
+        'UBtitles': '#086375',
+        'UBnone': '#bedcd0',
+        'UBfondo': '#f0dfc9',
+
+// 1	#f0dfc9	// Beige cálido, un poco más saturado
+// 2	#e5cbac	// Arena suave, tono crema oscuro
+// 3	#d8b894	// Marrón claro tipo terracota suave
+// 4	#c8a478	// Caramelo claro, más terroso
+// 5	#b68d5b	// Marrón tostado, cálido y sobrio
+
+
+
+        'UBop1': '#7A3B69',
+        'UBop2': '#5A746E',
       },
-       screens: {
-	'xsm' : '350px',
+      screens: {
+        'xsm': '350px',
         '3xl': '1736px', //pantallas desde 1736
         '4xl': '1900px', //pantallas desde 1900 la dell es 1920
         '5xl': '2350px', // pantallas desde 2350 y superior
       },
       maxWidth: {
-	'screen-xsm':'300px',
+        'screen-xsm': '300px',
         'screen-3xl': '1600px',
         'screen-4xl': '1700px',
         'screen-5xl': '2200px',
       },
     },
   },
-  plugins: [require('preline/plugin'),flowbite.plugin()],
+  plugins: [require('preline/plugin'), flowbite.plugin()],
 };
