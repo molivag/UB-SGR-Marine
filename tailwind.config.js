66c98cc16e78d37dfe3f73cdb196f2d07e29e0bb
molivag FedOX <maoliva@cimne.upc.edu>
2025-05-28 18:21:11 +0200

    tailwind.conf - se actualiza el screen xsm de 300 a 350px

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ export default {
         '5xl': '2350px', // pantallas desde 2350 y superior
       },
       maxWidth: {
-        'screen-xsm': '300px',
+        'screen-xsm': '350px',
         'screen-3xl': '1600px',
         'screen-4xl': '1700px',
         'screen-5xl': '2200px',
